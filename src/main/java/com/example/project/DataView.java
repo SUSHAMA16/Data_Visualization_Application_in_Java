3b5abe0b757dea68306eb4dc596cd984f967b02f
SUSHAMA16 <sushamakangude6@gmail.com>
2024-04-06 15:41:45 +0530

    Data Visualization in java

---
+++ src/main/java/com/example/project/DataView.java
@@ -0,0 +1,89 @@
+
+package com.example.project;
+
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DataView extends JFrame {
+
+    public DataView(String title) {
+        super(title);
+    }
+
+    public static void main(String[] args) {
+        // Prompt user for category names and values
+        String[] categories = new String[3];
+        double[] values = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            categories[i] = JOptionPane.showInputDialog("Enter category name " + (i + 1) + ":");
+            String valueInput = JOptionPane.showInputDialog("Enter value for category " + categories[i] + ":");
+            values[i] = Double.parseDouble(valueInput);
+        }
+
+        // Create a bar chart
+        DefaultCategoryDataset barDataset = new DefaultCategoryDataset();
+        for (int i = 0; i < 3; i++) {
+            barDataset.addValue(values[i], "Category", categories[i]);
+        }
+        JFreeChart barChart = ChartFactory.createBarChart(
+                "Bar Chart",
+                "Category",
+                "Value",
+                barDataset
+        );
+
+        // Create a pie chart
+        DefaultPieDataset pieDataset = new DefaultPieDataset();
+        for (int i = 0; i < 3; i++) {
+            pieDataset.setValue(categories[i], values[i]);
+        }
+        JFreeChart pieChart = ChartFactory.createPieChart(
+                "Pie Chart",
+                pieDataset,
+                true,
+                true,
+                false
+        );
+
+        // Create a line chart
+        XYSeries lineSeries = new XYSeries("Line Chart Data");
+        for (int i = 0; i < 3; i++) {
+            lineSeries.add(i + 1, values[i]);
+        }
+        XYSeriesCollection lineDataset = new XYSeriesCollection(lineSeries);
+        JFreeChart lineChart = ChartFactory.createXYLineChart(
+                "Line Chart",
+                "X",
+                "Y",
+                lineDataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        // Display all charts in the frame side by side
+        JPanel chartPanel = new JPanel(new GridLayout(1, 3));
+        chartPanel.add(new ChartPanel(barChart));
+        chartPanel.add(new ChartPanel(pieChart));
+        chartPanel.add(new ChartPanel(lineChart));
+
+        // Create and configure the frame
+        DataView project = new DataView("Data Visualization Project");
+        project.setContentPane(chartPanel);
+        project.setSize(1200, 600);
+        project.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        project.setVisible(true);
+    }
+}
